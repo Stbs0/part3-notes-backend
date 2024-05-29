9461251a4a0a6fe02c4a9c1e0eed61e85941e0de
Stbs0 <mohammedjrt@gmail.com>
2024-05-29 04:16:03 +0300

    hell

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+const cors = require("cors");
+app.use(express.static("dist"));
+app.use(cors());
 let notes = [
   {
     id: 1,
@@ -89,7 +92,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
